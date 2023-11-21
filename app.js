6e8717a27a01645619c859a52a972a0d740b649f
Emile0001 <boteseh@gmail.com>
2023-11-21 21:47:54 +0200

    two ways to create local server

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-// The main entry point for your Express application
-console.log("Hello world.");
+const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "application/json" });
+//     res.end(
+//         JSON.stringify({
+//             data: "Hello World!",
+//         })
+//     );
+// });
+
+// server.listen(3000);
+
+function handleRequest(req, res) {
+    res.statusCode = 200;
+    res.end("<h1>Hello World</h1>");
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000);
