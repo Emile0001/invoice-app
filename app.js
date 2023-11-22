08116e8771d4ce9e8189deeeafbf3757feb94bd1
Emile0001 <boteseh@gmail.com>
2023-11-22 18:28:02 +0200

    express method

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const http = require("http");
+// This method was actually suggested through auto complete
+// const http = require("http");
 
 // const server = http.createServer((req, res) => {
 //     res.writeHead(200, { "Content-Type": "application/json" });
@@ -11,16 +12,34 @@ const http = require("http");
 
 // server.listen(3000);
 
-function handleRequest(request, response) {
-    if (request.url === "/currenttime") {
-        response.statusCode = 200;
-        response.end("<h1>" + new Date().toISOString() + "</h1>");
-    } else if (request.url === "/") {
-        response.statusCode = 200;
+// 2nd method
+// const http = require("http");
+// function handleRequest(request, response) {
+//     if (request.url === "/currenttime") {
+//         response.statusCode = 200;
+//         response.end("<h1>" + new Date().toISOString() + "</h1>");
+//     } else if (request.url === "/") {
+//         response.statusCode = 200;
 
-        response.end("<h1>Hello World</h1>");
-    }
-}
-const server = http.createServer(handleRequest);
+//         response.end("<h1>Hello World</h1>");
+//     }
+// }
+// const server = http.createServer(handleRequest);
 
-server.listen(3000);
+// server.listen(3000);
+
+// Express method
+
+const express = require("express");
+
+const app = express();
+
+app.get("/currenttime", (req, res) => {
+    res.send("<h1>" + new Date().toISOString() + "</h1>");
+});
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World</h1>");
+});
+
+app.listen(3000);
